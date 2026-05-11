5c30bc4f6a0bb3d6474c12e24196ef6ed2d5a846
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2026-05-10 18:10:54 -0700

    feat: complete expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,9 +21,10 @@ function init() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 function getRecipesFromStorage() {
-	// A9. TODO - Complete the functionality as described in this function
-	//           header. It is possible in only a single line, but should
-	//           be no more than a few lines.
+	// A9. Complete the functionality as described in this function
+	//     header. It is possible in only a single line, but should
+	//     be no more than a few lines.
+	return Object.values(JSON.parse(localStorage.getItem('recipes')));
 }
 
 /**
@@ -34,11 +35,18 @@ function getRecipesFromStorage() {
  * @param {Array<Object>} recipes An array of recipes
  */
 function addRecipesToDocument(recipes) {
-	// A10. TODO - Get a reference to the <main> element
-	// A11. TODO - Loop through each of the recipes in the passed in array,
-	//            create a <recipe-card> element for each one, and populate
-	//            each <recipe-card> with that recipe data using element.data = ...
-	//            Append each element to <main>
+	// A10. Get a reference to the <main> element
+	let mainElement = document.querySelector('main');
+
+	// A11. Loop through each of the recipes in the passed in array,
+	//      create a <recipe-card> element for each one, and populate
+	//      each <recipe-card> with that recipe data using element.data = ...
+	//      Append each element to <main>
+	for (const recipe of recipes) {
+		let recipeElement = document.createElement('recipe-card');
+		recipeElement.data = recipe;
+		mainElement.append(recipeElement);
+	}
 }
 
 /**
